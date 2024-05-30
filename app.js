1345c82ea704d0e3ab8ca8b4122c408e64b6b89e
Seher Akşam <70154940+seheraksam@users.noreply.github.com>
2024-05-30 14:59:38 +0300

    second commit

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ const items=[1,[2,[3,[4]]]]
 const newItems=lodash.flattenDeep(items)
 
 console.log(newItems)
+ console.log("hello world")
--- http.js
+++ http.js
@@ -1,4 +1,17 @@
-const lodash = require('lodash');
-const items=[1,[2,[3,[4]]]]
-const newItems=_.flattenDeep(items)
-console.log(newItems)
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+      res.end('Welcome to our home page');
+    } else if (req.url === '/about') { // burada 'else if' kullanılmalı
+      res.end('Here is our short history');
+    } else {
+      res.end(`
+        <h1>Opps</h1>
+        <p>skdmsdmsdmsmdskmdksdksmdksd</p>
+        <a href="/">back home</a>
+      `);
+    }
+  });
+  
+  server.listen(5000);
+  
